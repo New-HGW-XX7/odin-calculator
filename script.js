1bd2464df45e1536fdb08ba904ccbcca7ec2f184
hgwxx7 <new_hgw_xx7@protonmail.com>
2022-06-17 14:16:30 +0200

    Add operate function

--- script.js
+++ script.js
@@ -22,6 +22,25 @@ function operate(num1, operator, num2) {
 
 // Functionality
 
+// Stores operands and operators in index-order: 0: number, 1: string, 2: number
+const opArray = [];
+
+// Performs the calculation
+function operate(num1, operator, num2) {
+    let temp;
+    switch (operator) {
+        case 'plus':
+            temp = add(num1, num2);
+            break;
+        
+        default:
+            console.log('default');
+    }
+    wipe(opArray);
+    opArray.push(temp);
+    writeIn(temp);
+}
+
 // Handles and populates the display
 function writeIn(input) {
     const output = document.querySelector('.output');
